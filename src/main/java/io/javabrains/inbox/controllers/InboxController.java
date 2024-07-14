11dd221ed8ef088c910a34d01bb1bc17899bb761
Ishwar749 <19itish083@ldce.ac.in>
2024-07-15 00:05:46 +0530

    Add folder display with layout

--- src/main/java/io/javabrains/inbox/controllers/InboxController.java
+++ src/main/java/io/javabrains/inbox/controllers/InboxController.java
@@ -2,6 +2,7 @@ package io.javabrains.inbox.controllers;
 
 import io.javabrains.inbox.folders.Folder;
 import io.javabrains.inbox.folders.FolderRepository;
+import io.javabrains.inbox.folders.FolderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -15,8 +16,9 @@ import java.util.List;
 @Controller
 public class InboxController {
 
-    @Autowired
-    private FolderRepository folderRepository;
+    @Autowired private FolderRepository folderRepository;
+    @Autowired private FolderService folderService;
+
 
     @GetMapping(value = "/")
     public String homePage(@AuthenticationPrincipal OAuth2User principal, Model model) {
@@ -28,6 +30,8 @@ public class InboxController {
         String userId = principal.getAttribute("login");
         List<Folder> userFolders= folderRepository.findAllById(userId);
         model.addAttribute("userFolders", userFolders);
+        List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
+        model.addAttribute("defaultFolders", defaultFolders);
         return "inbox-page";
     }
 }
